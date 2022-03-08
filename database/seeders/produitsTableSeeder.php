9d52f52ac7deec02938c2b3f0c13e8f640f8772f
Tsagadirts <tarektsagadirts@gmail.com>
2022-03-08 23:36:16 +0100

    first commit

---
+++ database/seeders/produitsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class produitsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $produit=new \app\produit();
+        $produit->nom="veste zara";
+        $produit->prix="25";
+        $produit->description="veste zara coton noir"
+        $produit->photo_principale=""
+    }
+}
